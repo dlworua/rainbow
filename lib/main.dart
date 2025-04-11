1f89ed40972ab6e6a6cc8edf29f90eb048377fb1
dlworua <leegaegyum@gmail.com>
2025-04-11 21:35:10 +0900

    feat: rainbow 홈페이지로 설정

--- lib/main.dart
+++ lib/main.dart
@@ -10,14 +10,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(home: Rainbow());
   }
 }
 
 class Rainbow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     throw UnimplementedError();
   }
 }
