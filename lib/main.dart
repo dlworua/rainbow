e5d5b647b49864d2316d519086663a61a56dd70c
dlworua <leegaegyum@gmail.com>
2025-04-13 16:41:03 +0900

    feat: 위젯 틀 생성

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,25 @@ class MyApp extends StatelessWidget {
 }
 
 class Rainbow extends StatelessWidget {
+  final List<Color> rainbowColors = [
+    Colors.red,
+    Colors.orange,
+    Colors.yellow,
+    Colors.green,
+    Colors.blue,
+    Colors.indigo,
+    Colors.purple,
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Column());
+    return SafeArea(
+      child: Column(
+        children:
+            rainbowColors.map((color) {
+              return Expanded(child: Container(color: color));
+            }).toList(),
+      ),
+    );
   }
 }
