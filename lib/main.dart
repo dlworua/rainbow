a3847390e2f1d4df1d5473be3233bc975def377c
dlworua <leegaegyum@gmail.com>
2025-04-13 17:04:55 +0900

    feat: 클릭시 각 색상에 대한 메세지 출력

--- lib/main.dart
+++ lib/main.dart
@@ -24,16 +24,30 @@ class Rainbow extends StatelessWidget {
     Colors.purple,
   ];
 
+  final List<String> colorNames = [
+    '빨간색',
+    '주황색',
+    '노란색',
+    '초록색',
+    '파란색',
+    '남색',
+    '보라색',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Column(
         children:
             List.generate(rainbowColor.length, (index) {
-              return Expanded(child : GestureDetector(onTap: () {
-               print('이건 ${colorNames[index]}이에요!') 
-              },)
-                child: Container(color: rainbowColor[index]));
+              return Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                    print('${colorNames[index]}을 선택하셨습니다.');
+                  },
+                  child: Container(color: rainbowColor[index]),
+                ),
+              );
             }).toList(),
       ),
     );
